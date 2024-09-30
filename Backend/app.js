a2b1b608473433bfb16234441b26860e07aa329f
maulikk04 <118893402+maulikk04@users.noreply.github.com>
2024-09-30 19:34:11 +0530

    Apis for login,signup,forgot and reset password

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,28 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const passportsetup = require('./middleware/passport_setup');
+const passport = require('passport')
+const session = require('express-session');
+const userRoutes = require('./routes/userRoutes');
 const app = express();
-
-app.use(cors());
-app.use(express.json());
-
 const uri = process.env.MONGO_URI;
-const port = 3000;
+const port = process.env.port || 4000;
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(cors({
+    exposedHeaders: ['Authorization']
+}));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth',userRoutes)
+
 mongoose.connect(uri)
 .then(()=>{
     console.log("Connected to Database");
