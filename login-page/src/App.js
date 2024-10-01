5fd44ee6f6ed1b5ee93c48aed74fb43d31bf82cf
Dev-yas <devvyasnoob@gmail.com>
2024-10-01 19:32:29 +0530

    landing page and login

---
+++ login-page/src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage';
+import UserTypeSelection from './components/UserTypeSelection';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/select-user" element={<UserTypeSelection />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
